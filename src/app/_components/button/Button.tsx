e5ae9137e7bb38bc191204449a27d1def86bc7df
mskim <wemb@wembui-Macmini.local>
2024-03-20 14:59:57 +0900

    feat: 버튼에 disabled 상태 추가

--- src/app/_components/button/Button.tsx
+++ src/app/_components/button/Button.tsx
@@ -6,12 +6,13 @@ type Props = {
   children: React.ReactNode;
   type?: "button" | "submit" | "reset";
   onClick: React.MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
 };
 
-const Button = ({ className = '', children, onClick, type = "button" }: Props) => {
+const Button = ({ className = '', children, onClick, type = "button", disabled = false }: Props) => {
   return (
     <div className={classNames(styled.button, className)}>
-      <button type={type} onClick={onClick}>{children}</button>
+      <button type={type} onClick={onClick} disabled={disabled}>{children}</button>
     </div>
   )
 };
