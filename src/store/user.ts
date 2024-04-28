94fa9ddc81f1a903498141de5d4fa3c1804f913b
suld2495 <suld7777@gmail.com>
2024-04-28 16:17:53 +0900

    feat: 유저 결과 정렬

---
+++ src/store/user.ts
@@ -0,0 +1,28 @@
+import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
+import { Group } from "./generator";
+import { User } from "@/app/_lib/group";
+
+export type GroupUser = {
+  name: User;
+  group: Group;
+}
+
+type UserState = {
+  required: GroupUser[];
+};
+
+const useUserStore = create<UserState>()(
+  devtools(
+    persist(
+      (_set, _get) => ({
+        required: [],
+      }),
+      {
+        name: "user",
+      },
+    )
+  )
+);
+
+export default useUserStore;
